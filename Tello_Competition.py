16560e4581606cb61e09ed8622bcc3d94abbf3c7
Hayven Baarson <hcbaarson@outlook.com>
2025-04-09 09:10:44 -0400

    Update Tello_Competition.py

--- Tello_Competition.py
+++ Tello_Competition.py
@@ -73,7 +73,7 @@ try:
         sendmsg('curve -155 -155 0 0 -290 0 60')
 
         #Commit Message: Final Hoop - Stable
-        sendmsg('go -230xx 0 -70 100')
+        sendmsg('go -230 0 -70 100')
 
         sendmsg('land')
         sendmsg('battery?')
